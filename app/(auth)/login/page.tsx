db06dab75fe50e1092863e2bfd2fcc2d77af6303
Aji Prio <ajiprio@fleetify.id>
2025-11-23 20:25:53 +0700

    fix: added suspense wrapper

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import {GalleryVerticalEnd} from "lucide-react";
 import {LoginForm} from "./_components/login.form";
 import Image from "next/image";
+import {Suspense} from "react";
 
 export default function LoginPage() {
     return (
@@ -8,7 +9,9 @@ export default function LoginPage() {
             <div className="flex flex-col gap-4 p-6 md:p-10">
                 <div className="flex flex-1 items-center justify-center">
                     <div className="w-full max-w-xs">
-                        <LoginForm />
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <LoginForm />
+                        </Suspense>
                     </div>
                 </div>
             </div>
